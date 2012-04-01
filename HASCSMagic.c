5075f91e7d6611e42916dc740b788f97116dd99a
Peter Backes <rtc@helen.PLASMA.Xg8.DE>
2012-04-01 21:36:08 +0200

    *** empty log message ***

---
+++ HASCSMagic.c
@@ -0,0 +1,169 @@
+IMPLEMENTATION MODULE HASCSMagic;
+
+
+int Vision(unsigned Weite, unsigned Art, unsigned *lx, unsigned *ly)
+{
+#define MaxWeite 44
+
+	unsigned VAR xl, yl, Anzahl, RahmenX, RahmenY, RahmenBreite,
+		m, mx, my, x, y;
+	SpriteType Sprite;
+	BITSET mb;
+	char ch;
+	int XOff, YOff;
+
+	void ZeigeFeld(unsigned x, unsigned y)
+	{
+		int zeigen;
+		int x1, y1;
+		unsigned xk, yk, Farbe;
+
+		zeigen = TRUE;
+		x1 = XOff + x + Spieler.x - (Anzahl / 2);
+		y1 = YOff + y + Spieler.y - (Anzahl / 2);
+		if (LevelNotZyklisch & LevelFlags) { /* nicht zyklisch */
+			zeigen = x1 >= 0 && x1 <= LevelBreite
+				&& y1 >= 0 && y1 <= LevelHoehe);
+		}
+		NormalKoords(x1, y1, xl, yl);
+		if ((1<<0) & Art) { /* Karte */
+			zeigen = zeigen && LevelKarte & Level[xl][yl].Spezial;
+		}
+		xk = RahmenX * 4 + 4 + x;
+		yk = RahmenY * 4 + 4 + y;
+		if (zeigen) {
+			Farbe = Felder[Level[xl][yl].Feld].Spezial * 16;
+			SetSpritePart(xk, yk, Farbe, Sprite);
+		} else
+			SetSpritePart(xk, yk, 0, SystemSprite[0]);
+	}
+
+	Sprite = SystemSprite[Art * 2 + 34];
+
+	if (Weite > MaxWeite) Weite = MaxWeite;
+	m = LevelBreite;
+	if (m < LevelHoehe) m = LevelHoehe;
+	if (LevelNotZyklisch & LevelFlags) {
+		if (Weite > m) Weite = m;
+	} else
+		if (Weite > m / 2) Weite = m / 2;
+
+	RahmenBreite = Weite / 2 + 2;
+	RahmenY = 11 - Weite / 4;
+	RahmenX = 18 - Weite / 4;
+	ReserveScreen(RahmenX, RahmenY,
+		RahmenX + RahmenBreite, RahmenY + RahmenBreite);
+	Anzahl = (RahmenBreite - 1) * 4;
+	XOff = 0; YOff = 0;
+	for (x = 0; x <= Anzahl-1; x++)
+		for (y = 0; y <= Anzahl-1; y++)
+			ZeigeFeld(x, y);
+	if ((1<<1) & Art) /* keine Verschiebung */
+		WaitInput(mx, my, mb, ch, -1);
+	else {
+		do {
+			WaitInput(mx, my, mb, ch, -1);
+			if (mb) {
+				x = mx / 16; y = my / 16;
+				if (x == RahmenX && y >= RahmenY
+				 && y <= RahmenY + RahmenBreite)
+					ch = '4';
+				else if (x == RahmenX + RahmenBreite && y >= RahmenY
+					&& y <= RahmenY + RahmenBreite)
+					ch = '6';
+				else if (y == RahmenY && x >= RahmenX 
+					&& x <= RahmenX + RahmenBreite)
+					ch = '8';
+				else if (y == RahmenY + RahmenBreite && x >= RahmenX
+					&& x <= RahmenX + RahmenBreite)
+					ch = '2';
+			}
+			if (ch == '6' || ch == '9' || ch == '3') {
+				Copy(3, (RahmenX+1)*16+4, (RahmenY+1)*16,
+					(RahmenBreite-1)*16-4, (RahmenBreite-1)*16,
+					(RahmenX+1)*16, (RahmenY+1)*16);
+				XOff++;
+				for (x = 0; x <= Anzahl-1; x++)
+					ZeigeFeld(Anzahl-1, x);
+			}
+			if (ch == '1' || ch == '4' || ch == '7') {
+				Copy(3, (RahmenX+1)*16, (RahmenY+1)*16,
+					(RahmenBreite-1)*16-4, (RahmenBreite-1)*16,
+					(RahmenX+1)*16+4, (RahmenY+1)*16);
+				XOff--;
+				for (x = 0; x <= Anzahl-1; x++)
+					ZeigeFeld(0, x);
+			}
+			if (ch == '7' || ch == '8' || ch == '9') {
+				Copy(3, (RahmenX+1)*16, (RahmenY+1)*16,
+					(RahmenBreite-1)*16, (RahmenBreite-1)*16-4,
+					(RahmenX+1)*16, (RahmenY+1)*16+4);
+				YOff--;
+				for (x = 0; x <= Anzahl-1; x++)
+					ZeigeFeld(x, 0);
+			}
+			if (ch == '1' || ch == '2' || ch == '3') {
+				Copy(3, (RahmenX+1)*16, (RahmenY+1)*16+4,
+					(RahmenBreite-1)*16, (RahmenBreite-1)*16-4,
+					(RahmenX+1)*16, (RahmenY+1)*16);
+				YOff++;
+				for (x = 0; x <= Anzahl-1; x++)
+					ZeigeFeld(x, Anzahl-1);
+			}
+			NewXMin = RahmenX; NewYMin = RahmenY;
+			NewXMax = RahmenX + RahmenBreite; NewYMax = RahmenY + RahmenBreite;
+		} while (ch >= '1' && ch <= '9');
+	}
+	if (mb)
+		if (mx >= (RahmenX+1)*16 && mx < (RahmenX+RahmenBreite-1) * 16
+		 && my >= (RahmenY+1)*16 && my < (RahmenY+RahmenBreite-1) * 16)
+		{
+			mx = (mx - (RahmenX+1)*16) / 4;
+			my = (my - (RahmenY+1)*16) / 4;
+			NormalKoords((int)(Spieler.x+mx) - Anzahl / 2 + XOff,
+				(int)(Spieler.y+my) - Anzahl / 2 + YOff, lx, ly);
+			return TRUE;
+		}
+	return FALSE;
+}
+
+void Todeshauch(unsigned Weite, unsigned Wirkung)
+{
+	unsigned i, x, y, xl, yl; int b;
+	for (x = MaxSichtweite-Weite; x <= MaxSichtweite+Weite; x++)
+		for (y = MaxSichtweite-Weite; y <= MaxSichtweite+Weite; y++)
+			InvertFeld(x+1, y+1);
+	WaitTime(0);
+	for (x = MaxSichtweite-Weite; x <= MaxSichtweite+Weite; x++)
+		for (y = MaxSichtweite-Weite; y <= MaxSichtweite+Weite; y++) {
+			SichtLevelUmrechnung(x, y, xl, yl);
+			if (LevelMonster & Level[xl][yl].Spezial) {
+				i = FindMonster(xl, yl);
+				if (MonsterImmun & ~Monster[i].Spezial)
+					b = HitMonster(Monster[i], W6(Wirkung));
+			}
+		}
+	for (x = MaxSichtweite-Weite; x <= MaxSichtweite+Weite; x++)
+		for (y = MaxSichtweite-Weite; y <= MaxSichtweite+Weite; y++)
+			InvertFeld(x+1, y+1);
+}
+
+void Erweckung(unsigned NeuerStatus)
+{
+	unsigned i, nummer, min, Entfernung;
+	nummer = 0; i = 1; min = UINT_MAX;
+	while (i <= AnzahlMonster) {
+		if (Monster[i].Status == 0) {
+			Entfernung = ABS(Spieler.x) - (int)Monster[i].x)
+				+ ABS(Spieler.y) - (int)Monster[i].y);
+			if (Entfernung < min) {
+				min = Entfernung;
+				nummer = i;
+			}
+		}
+		i++;
+	}
+	if (nummer != 0)
+		Monster[nummer].Status = NeuerStatus;
+}
+
---
+++ Start.c
@@ -0,0 +1,177 @@
+IMPLEMENTATION MODULE Start;
+
+unsigned Auswahl, SpriteAnzahl, d, s;
+int Ok, OnceMore;
+
+
+int ReadConfig()
+	char Path[129];
+
+	void MakeAbsolute(char *p)
+	{
+		if (p[1] == ':') return;
+		Concat(p, ActPath, p);
+	}
+
+	ShowError = FALSE;
+	do {
+		if (Length(Command) == 0)
+			Assign(Command, "HASCS.CFG");
+		DiaPath = "DIALOG\\";
+		MapPath = "MAP\\";
+		PlaPath = "SAVE\\";
+		PrgPath = "SYSTEM\\";
+		SoundPath = "SOUND\\";
+		TextEditor = "EDITOR.PRG";
+		IniFile = "HASCS.INI";
+		if (LoadDialog(ConfigDialog, FALSE)) {
+			ExecuteDialog();
+			MakeAbsolute(DiaPath);
+			MakeAbsolute(MapPath);
+			MakeAbsolute(PlaPath);
+			MakeAbsolute(PrgPath);
+			MakeAbsolute(SoundPath);
+		}
+		LoadLevel(1);
+		if (FileError) {
+			if (!SelectFile("Welches Abenteuer...", "*.CFG", Command))
+				return FALSE;
+		}
+	} while (FileError);
+	ShowError = TRUE;
+	return TRUE;
+}
+
+
+unsigned Anfangsmaske(void)
+{
+	unsigned d, x, wahl;
+	NewScreen(8, 3, 24, 17, " HASCS III ");
+	d = AddObject(2, 2, 20, 1, "#100#", BigText);
+	d = AddObject(2, 3, 20, 1, "#101#", BigText);
+	d = AddObject(2, 4, 20, 1, "#102#", BigText);
+	d = AddObject(2, 2, 20, 1, "#103#", 0);
+	d = AddObject(2, 3, 20, 1, "#104#", 0);
+	d = AddObject(2, 4, 20, 1, "#105#", 0);
+	d = AddObject(2, 6, 20, 1, "353", SpriteFill);
+	x = AddObject(2, 8, 20, 1, "#111#", BigText|Outlined|Exit);
+	d = AddObject(2,10, 20, 1, "#112#", BigText|Outlined|Exit);
+	d = AddObject(2,12, 20, 1, "#113#", BigText|Outlined|Exit);
+	d = AddObject(2,14, 20, 1, "#114#", BigText|Outlined|Exit);
+
+	DrawScreen();
+	wahl = HandleScreen();
+	RestoreScreen();
+	
+	return wahl + 1 - x;
+}
+
+
+void PlayTheGame(unsigned Wie)
+{
+	unsigned i;
+	int Ende;
+
+	for (i = 1; i <= 7; i++)
+		OutputText(" "); /* Textzeilen löschen */
+	OutputText("#109#");
+
+	LoadLevel(Spieler.LevelNumber);
+	if (Wie == 1 || Wie == 3) {
+		Sichtweite = SetLightRange();
+		Spieler.x = Spieler.LevelBreite / 2;
+		Spieler.y = Spieler.LevelHoehe / 2;
+		DoDialog(0); /* Anfangsdialog */
+		LoadOrSavePlayer(FALSE); /* Anfangsabspeichern */
+	}
+	PrintCharakter(0);
+
+	do {
+		Ende = TRUE;
+		switch (DoGame()) {
+		case 1 : /* draufgegangen */
+			DoDialog(999); /* Todesdialog */
+			Ende = STot & Spieler.Status;
+			break;
+		case 3 : /* geschafft ! */
+			DoDialog(998);
+			DeleteLevels;
+			LoadOrSavePlayer(FALSE);
+			break;
+		}
+	} while (!Ende);
+}
+
+
+void StartGame();
+{
+
+	Editor = FALSE;
+	DruckerAusgabe = FALSE;
+	DebugMode = FALSE;
+	SoundAusgabe = TRUE;
+	MaxX = 78; /* Ausgabebreite */
+
+	DoDialog(IniDialog); /* Texte, Anfangsbild etc.*/
+
+	Spieler.x = LevelBreite / 2;
+	Spieler.y = LevelHoehe / 2;
+	Spieler.Sichtweite = SetLightRange();
+	Spieler.Typ = 1<<0; Spieler.Sprite = 10;
+
+	MakeSichtBereich(TRUE);
+	RestoreScreen();
+
+	do {
+		OutputText("                            ");
+		OutputText("                            ");
+		OutputText("          HASCS III         ");
+		OutputText("         Version 1.43       ");
+		OutputText("                            ");
+		OutputText("    Copyright © 1987-1995   ");
+		OutputText("      Alexander Kirchner    ");
+		OutputText("                            ");
+		
+		Auswahl = Anfangsmaske();
+
+		if (Auswahl <= 3) {
+			NewScreen(8, 7, 23, 7, "");
+			d = AddObject(2, 2, 19, 1, "#106#", BigText);
+			s = AddObject(2, 4, 19, 1, "", {Editable, Outlined, BigText});
+			SetInputString(s, Spieler.Name);
+			DrawScreen(); d = HandleScreen(); RestoreScreen();
+			GetInputString(s, Spieler.Name);
+			Ok = FALSE;
+			if (Compare(Spieler.Name, ""))
+				Auswahl = 0;
+
+			if (Auswahl == 1) { 
+				Ok = CreateCharakter(Spieler);
+				if (Ok)
+					DeleteLevels();
+			}
+
+			if (Auswahl == 2) {
+				LoadOrSavePlayer(TRUE);
+				Ok = !FileError;
+				if (Spieler.LevelNumber == 0 || Spieler.LevelNumber > 999)
+					Auswahl = 3;
+			}
+
+			if (Auswahl == 3) {
+				LoadOldPlayer();
+				Ok = !FileError;
+				if (Ok)
+					DeleteLevels();
+			}
+
+			if (Ok) {
+				DebugMode = BenutzerNummer == StringToCard(Spieler.Name);
+				PlayTheGame(Auswahl);
+			}
+
+		} /* if (Auswahl... */
+
+	} while (Auswahl != 4);
+
+}
