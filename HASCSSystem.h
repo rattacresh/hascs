e9a3358e1bbb11a18f86dfce84391561afb50c54
Peter Backes <rtc@helen.PLASMA.Xg8.DE>
2012-04-04 18:07:06 +0200

    fix mouse buttons again

--- HASCSSystem.h
+++ HASCSSystem.h
@@ -27,8 +27,8 @@
            11.0  13.10.94 GetBuffer, Cache
 */
 
-#define MausLinks   0 /* linker Mausknopf */
-#define MausRechts  1 /* rechter Mausknopf */
+#define MausLinks   (1<<0) /* linker Mausknopf */
+#define MausRechts  (1<<1) /* rechter Mausknopf */
 #define MaxCache  100
 
 typedef struct {
